d37c5080259e2b31e73410463bac6e6ebfda492e
vipacheco <isc.antoniozapata@gmail.com>
2017-11-15 13:01:25 -0600

    Se movieron archivos del proyecto y se agrego la vista dashboard.

---
+++ gestic/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gestic.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Index";
+
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
